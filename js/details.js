2bcabfd8de7263f6643bfb04da1111a685d2958d
Mr-Digi <43848126+Mr-Digi@users.noreply.github.com>
2023-04-02 15:39:38 +0200

    Added details css and contact form

--- js/details.js
+++ js/details.js
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 
 const loadingIndicator = document.createElement('div');
-const wantedHolder = document.getElementsByClassName("wanted")
+const wantedHolder = document.getElementsByClassName("details")
 const ul = document.createElement('ul');
 const img = document.createElement('img');
 loadingIndicator.textContent = 'Loading...';
@@ -14,24 +14,80 @@ fetch(apiUrl)
         console.log(data)
         const ul = document.createElement('ul');
         document.title = data.title
-        const liName = document.createElement('li');
-        const liDescription = document.createElement('li');
-        const liReward = document.createElement('li');
+        
+        const nameDiv = document.createElement('div')
+        nameDiv.setAttribute("class", 'detailDiv')
+
+        const nameTitle = document.createElement('b');
+        const wantedName = document.createElement('p');
+
+        const occupationTitle = document.createElement('b');
+        const wantedOccupation = document.createElement('p');
+
+        const sexTitle = document.createElement('b');
+        const wantedSex = document.createElement('p');
+
+        const raceTitle = document.createElement('b');
+        const wantedRace = document.createElement('p');
+        
+        const descriptionTitle = document.createElement('b');
+        const wantedDescription = document.createElement('p');
+        
+        const rewardTitle = document.createElement('b');
+        const wantedReward = document.createElement('p');
+        
+        const textHolder = document.createElement('li');
         const liImage = document.createElement('img');
         liImage.src = data.images[0].original;
-        liName.textContent = 'Name: ' + data.title;
-        liDescription.textContent = 'Description: ' + data.description;
-        //Check if there is a reward. If there is, display it. if not, display "No reward"
-        if (data.reward_text) {
-            liReward.textContent = 'Reward: ' + data.reward_text;
+
+        nameTitle.textContent = "Name:  ";
+        wantedName.textContent = data.title;
+
+        occupationTitle.textContent = "Work:  ";
+        if (data.occupations) {
+            wantedOccupation.textContent = data.occupations[0];
         } else {
-            liReward.textContent = 'Reward: No reward';
+            wantedOccupation.textContent = "Unknown";
         }
-    
-        ul.appendChild(liName);
-        ul.appendChild(liDescription);
-        ul.appendChild(liReward)
+
+        sexTitle.textContent = "Sex:  ";
+        wantedSex.textContent = data.sex;
+
+        raceTitle.textContent = "Race:  ";
+        wantedRace.textContent = data.race;
+
+        descriptionTitle.textContent = "Descripton: ";
+        wantedDescription.textContent = data.description;
+        
+        rewardTitle.textContent = "Reward: ";
+        if (data.reward_text) {
+            wantedReward.textContent = data.reward_text;
+        } else {
+            wantedReward.textContent = 'No reward';
+        }
+
+        nameDiv.appendChild(nameTitle);
+        nameDiv.appendChild(wantedName);
+
+        nameDiv.appendChild(occupationTitle);
+        nameDiv.appendChild(wantedOccupation);
+
+        nameDiv.appendChild(sexTitle);
+        nameDiv.appendChild(wantedSex);
+
+        nameDiv.appendChild(raceTitle);
+        nameDiv.appendChild(wantedRace);
+
+        nameDiv.appendChild(descriptionTitle);
+        nameDiv.appendChild(wantedDescription);
+
+        nameDiv.appendChild(rewardTitle);
+        nameDiv.appendChild(wantedReward);
+
+        textHolder.appendChild(nameDiv);
+
         ul.appendChild(liImage);
+        ul.appendChild(textHolder);
         wantedHolder[0].appendChild(ul);
         document.body.removeChild(loadingIndicator);
     })
