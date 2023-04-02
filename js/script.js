902dd86cd8b9ba12c7c349915d734e0326330dad
Mr-Digi <43848126+Mr-Digi@users.noreply.github.com>
2023-04-02 13:53:52 +0200

    Added css to main page

--- js/script.js
+++ js/script.js
@@ -5,34 +5,38 @@ const loadingIndicator = document.createElement('div');
 const wantedHolder = document.getElementsByClassName("wanted")
 loadingIndicator.textContent = 'Loading...';
 document.body.appendChild(loadingIndicator);
+try {
+    fetch(apiUrl)
+        .then(response => response.json()) //Convert the response to Json
+        .then(data => {
+            console.log(data)
+            for (let i = 0; i < data.items.length; i++) {
+                const ul = document.createElement('ul');
+                const image = document.createElement('img');
+                const link = document.createElement('a');
+                image.src = data.items[i].images[0].thumb
+                link.href = 'details.html?wanted='+data.items[i].uid
+                link.appendChild(image)
 
-fetch(apiUrl)
-    .then(response => response.json()) //Convert the response to Json
-    .then(data => {
-        console.log(data)
-        for (let i = 0; i < data.items.length; i++) {
-            const ul = document.createElement('ul');
-            const image = document.createElement('img');
-            const link = document.createElement('a');
-            image.src = data.items[i].images[0].thumb
-            link.href = 'details.html?wanted='+data.items[i].uid
-            link.appendChild(image)
-
-            const liName = document.createElement('li');
-            const liValue = document.createElement('li');
-            const liImage = document.createElement('li');
-            liName.textContent = data.items[i].title;
-            liValue.textContent = data.items[i].description;
-            liImage.appendChild(link);
-            link.appendChild(liName)
-            ul.appendChild(link);
-            ul.appendChild(liValue);
-            ul.appendChild(liImage);
-            wantedHolder[0].appendChild(ul);
-        }
-        document.body.removeChild(loadingIndicator);
-    })
-    .catch(error => {
-        console.error(error);
-        document.body.removeChild(loadingIndicator);
-    })
+                const liName = document.createElement('li');
+                const liValue = document.createElement('li');
+                const liImage = document.createElement('li');
+                liName.textContent = data.items[i].title;
+                liValue.textContent = data.items[i].description;
+                liImage.appendChild(link);
+                link.appendChild(liName)
+                ul.appendChild(link);
+                ul.appendChild(liValue);
+                ul.appendChild(liImage);
+                wantedHolder[0].appendChild(ul);
+            }
+            document.body.removeChild(loadingIndicator);
+        })
+        .catch(error => {
+            console.error(error);
+            document.body.removeChild(loadingIndicator);
+        })
+} catch(error) {
+    console.log('An error occured')
+    document.body.removeChild(loadingIndicator);
+}
